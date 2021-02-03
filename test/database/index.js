8700435aa4e00af3853d63ffc4e47c4a590434fe
norealy <xdatgd@gmail.com>
2021-02-03 11:54:54 -0500

    NEOS_VN_BNT-19 Fix commit 2

--- test/database/index.js
+++ test/database/index.js
@@ -1,7 +1,7 @@
 const { Model } = require('objection');
 const Env = require('../../utils/Env');
 const Knex = require('knex');
-const configDB = require('../../common/utils/mysql/Database')(Env.get('PWD', __dirname));
+const configDB = require('../../common/utils/mysql/Database')(Env.appRoot);
 
 const mysql = Knex(configDB);
 Model.knex(mysql);

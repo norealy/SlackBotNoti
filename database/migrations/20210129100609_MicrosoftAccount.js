72870206e9205e84d33a229156c41254c27037e9
norealy <xdatgd@gmail.com>
2021-02-08 09:13:55 -0500

    NEOS_VN_BNT-35 Fix type refresh token

--- database/migrations/20210129100609_MicrosoftAccount.js
+++ database/migrations/20210129100609_MicrosoftAccount.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('microsoft_account', function(table) {
         table.string('id').primary().notNull();
         table.string('name').notNull();
-        table.string('refresh_token').notNull();
+        table.text('refresh_token').notNull();
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table.timestamp('updated_at').defaultTo(knex.fn.now());
     });

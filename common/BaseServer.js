3b9b44c40ec2c8b7e8904c926ddbd53292326d74
Tuấn Nguyễn <tuannv@neoscorp.vn>
2021-02-25 21:39:56 +0700

    NEOS_VN_BNT-38 update login wrapper

--- common/BaseServer.js
+++ common/BaseServer.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const Express = require('express');
 const knex = require("knex");
-const { Model } = require('objection');
+const {Model} = require('objection');
 const Path = require('path');
-const Fs = require('fs-extra');
+const FsExtra = require('fs-extra');
+const Fs = require('fs');
 const BodyParser = require('body-parser');
 const _ = require("lodash");
 const Env = require('../utils/Env');
@@ -17,57 +18,83 @@ class BaseServer {
 	 * @param {string} instanceId
 	 * @param {object} opt
 	 */
-  constructor(instanceId, opt) {
-		const args  = instanceId.split("-");
+	constructor(instanceId, opt) {
+		const args = instanceId.split("-");
 
 		this.app = Express();
 		this.instanceId = instanceId;
 		this.chatService = args[0];
 		this.resourceServer = args[1];
 
-    this.config = typeof opt.config === "object" ? opt.config : {};
+		this.config = typeof opt.config === "object" ? opt.config : {};
 
 		this.chatServiceHandler = this.chatServiceHandler.bind(this);
 		this.resourceServerHandler = this.resourceServerHandler.bind(this);
 		this.pushMessageHandler = this.pushMessageHandler.bind(this);
 
-    this.requestHandler = {
+		this.requestHandler = {
 			watchChatService: this.chatServiceHandler,
-      watchResourceServer: this.resourceServerHandler,
-      pushMessage: this.pushMessageHandler,
-    }
-  }
+			watchResourceServer: this.resourceServerHandler,
+			pushMessage: this.pushMessageHandler,
+		}
+	}
 
 	/**
 	 * Read config file from config folder(either depending on the service you want to run)
 	 * @param fileName
 	 * @returns {Promise<unknown>}
 	 */
-  getConfig(fileName) {
-    return new Promise((resolve, reject) => {
-      Fs.readJson(Path.join(this.config.path, fileName), (err, text) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(text);
-        }
-      });
-    });
-  }
+	getConfig(fileName) {
+		return new Promise((resolve, reject) => {
+			FsExtra.readJson(Path.join(this.config.path, fileName), (err, text) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(text);
+				}
+			});
+		});
+	}
 
 	/**
 	 * Load config from file in config folder(either depending on the service you want to run)
 	 * @param {string} name
 	 * @return {Promise<unknown>}
 	 */
-  loadConfig(name) {
-    try{
-      const fileName = Path.join(name + '.json');
-      return this.getConfig(fileName)
-    } catch (err) {
+	loadConfig(name) {
+		try {
+			const fileName = Path.join(name + '.json');
+			return this.getConfig(fileName)
+		} catch (err) {
 			throw new Error(`E_LOAD_CONFIG: ${err.message}`)
-    }
-  }
+		}
+	}
+
+	/**
+	 * Load config from file in config folder(either depending on the service you want to run)
+	 * @param {object} wrapper
+	 * @return {object}
+	 */
+	async loadConfigWrapper(wrapper) {
+		const listFile = Fs.readdirSync(this.config.path);
+		listFile.splice(_.findIndex(listFile,(value) => value === `WRAPPER.json`),
+			1);
+		listFile.splice(_.findIndex(listFile,(value) => value === `${this.chatService}.json`),
+			1);
+		for (let i = 0, length = listFile.length; i < length; i++) {
+			const regex = /.json$/;
+			if (regex.test(listFile[i])) {
+				const prefix = listFile[i].substr(0, 2);
+				const config = await this.getConfig(listFile[i]);
+				const props = Object.keys(config.resourceServer);
+				for (let j = 0, lngProps = props.length; j < lngProps; j++) {
+					wrapper.resourceServer[`${prefix}_${props[j]}`] = config.resourceServer[`${props[j]}`]
+				}
+				wrapper.server[`${listFile[i].replace(regex, "")}_PORT`] = config.server["PORT"]
+			}
+		}
+		return wrapper
+	}
 
 	/**
 	 * Check instance environment variables and initialize the environment object
@@ -76,26 +103,22 @@ class BaseServer {
 	 */
 	configEnv(instanceEnv) {
 		const message = "object does not exist in the config file or it is not an object"
-		if(!instanceEnv.server
+		if (!instanceEnv.server
 			|| typeof instanceEnv.server !== "object"
 			|| _.isArray(instanceEnv.server)) {
 			throw new Error(`E_CONFIG_ENVIRONMENT: The server ${message}`);
 		}
-		if(!instanceEnv.chatService
+		if (!instanceEnv.chatService
 			|| typeof instanceEnv.chatService !== "object"
 			|| _.isArray(instanceEnv.chatService)) {
 			throw new Error(`E_CONFIG_ENVIRONMENT: The chatService ${message}`);
 		}
-		if(!instanceEnv.resourceServer
+		if (!instanceEnv.resourceServer
 			|| typeof instanceEnv.resourceServer !== "object"
 			|| _.isArray(instanceEnv.resourceServer)) {
 			throw new Error(`E_CONFIG_ENVIRONMENT: The resourceServer ${message}`);
 		}
-		try {
-			Env.setConfig(instanceEnv);
-		} catch (e){
-			throw e;
-		}
+		Env.setConfig(instanceEnv);
 	}
 
 	/**
@@ -112,7 +135,7 @@ class BaseServer {
 					return resolve(1)
 				})
 				.catch((err) => {
-					const code ="E_ACCESS_MYSQL_ERROR";
+					const code = "E_ACCESS_MYSQL_ERROR";
 					const message = err.sqlMessage ? err.sqlMessage : 'Access to mysql database denied';
 					const error = new Error(`${code}: ${message}`);
 					error.code = code;
@@ -121,7 +144,7 @@ class BaseServer {
 		})
 	}
 
-	configRedis(){
+	configRedis() {
 		return new Promise((resolve, reject) => {
 			const connectSuccess = () => {
 				console.log("redis connect success");
@@ -144,53 +167,56 @@ class BaseServer {
 		})
 	}
 
-  healthCheckHandler(req, res, next) {
-    try {
-      return res.status(200).send({"pong":"OK"});
-    } catch (e) {
-      return res.status(400);
-    }
-  }
+	healthCheckHandler(req, res, next) {
+		try {
+			return res.status(200).send({"pong": "OK"});
+		} catch (e) {
+			return res.status(400);
+		}
+	}
 
-  chatServiceHandler(req, res, next) {
-    return res.status(200).send("OK");
-  }
+	chatServiceHandler(req, res, next) {
+		return res.status(200).send("OK");
+	}
 
-  resourceServerHandler(req, res, next) {
-    return res.status(200).send("OK");
-  }
+	resourceServerHandler(req, res, next) {
+		return res.status(200).send("OK");
+	}
 
 	pushMessageHandler(req, res, next) {
-    return res.status(200).send("OK");
-  }
+		return res.status(200).send("OK");
+	}
 
-  async init() {
-		const instanceEnv = await this.loadConfig(this.resourceServer);
+	async init() {
+		let instanceEnv = await this.loadConfig(this.resourceServer);
+		if (this.resourceServer === "WRAPPER") {
+			instanceEnv = await this.loadConfigWrapper(instanceEnv);
+		}
 		instanceEnv.chatService = await this.loadConfig(this.chatService);
 		this.configEnv(instanceEnv);
 
-    if(this.resourceServer === "WRAPPER" || /dev/.test(Env.get("NODE_ENV", "dev"))){
-    	const mongoDB = await Mongo();
+		if (this.resourceServer === "WRAPPER" || /dev/.test(Env.get("NODE_ENV", "dev"))) {
+			const mongoDB = await Mongo();
 			const logModel = LogModel(mongoDB);
-    	require('../utils/logger')(this.app, this.instanceId, logModel)
-    }
+			require('../utils/logger')(this.app, this.instanceId, logModel)
+		}
 
 		await this.configMySQL();
 		await this.configRedis();
 
-    this.app.disable('x-powered-by');
+		this.app.disable('x-powered-by');
 		this.app.use(BodyParser.urlencoded({extended: true, limit: '2mb'}));
-		this.app.use(BodyParser.json({ limit: '2mb' }));
+		this.app.use(BodyParser.json({limit: '2mb'}));
 
-    this.app.get('/health-check', this.healthCheckHandler);
-    this.app.post('/watch/chat-service', this.requestHandler.watchChatService);
-    this.app.post('/watch/resource-server', this.requestHandler.watchResourceServer);
-    this.app.post('/push/message', this.requestHandler.pushMessage);
+		this.app.get('/health-check', this.healthCheckHandler);
+		this.app.post('/watch/chat-service', this.requestHandler.watchChatService);
+		this.app.post('/watch/resource-server', this.requestHandler.watchResourceServer);
+		this.app.post('/push/message', this.requestHandler.pushMessage);
 
-    this.app.listen(Env.serverGOF("PORT"), () => {
-      console.log('%s listening at %s', this.instanceId, Env.serverGOF("PORT"));
-    });
-  }
+		this.app.listen(Env.serverGOF("PORT"), () => {
+			console.log('%s listening at %s', this.instanceId, Env.serverGOF("PORT"));
+		});
+	}
 
 }
 
--- slack/wrapper/index.js
+++ slack/wrapper/index.js
@@ -1,12 +1,22 @@
 const BaseServer = require('../../common/BaseServer');
 const httpProxy = require('http-proxy');
+const Axios = require('axios');
+const Template = require("../views/Template");
+const Env = require("../../utils/Env");
+const Channels = require("../../models/Channels");
+
+const {
+	handlerOptionLogin,
+	configUrlAuthGoogle,
+	configUrlAuthMicrosoft,
+} = require("./ChatService");
 
 const proxy = httpProxy.createProxyServer({});
 
-proxy.on('proxyReq', function(proxyReq, req) {
-	if(req.body) {
+proxy.on('proxyReq', function (proxyReq, req) {
+	if (req.body) {
 		let bodyData = JSON.stringify(req.body);
-		proxyReq.setHeader('Content-Type','application/json');
+		proxyReq.setHeader('Content-Type', 'application/json');
 		proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
 		proxyReq.write(bodyData);
 	}
@@ -15,13 +25,51 @@ proxy.on('proxyReq', function(proxyReq, req) {
 class SlackWrapper extends BaseServer {
 	constructor(instanceId, opt) {
 		super(instanceId, opt);
+		this.loginWrapper = this.loginWrapper.bind(this);
+		this.template = Template();
 	}
 
-	chatServiceHandler(req, res, next) {
-		return res.status(200).send("Watch chat service handler");
+	/**
+	 *
+	 * @param {array} view
+	 * @param {object} event
+	 * @returns {Promise}
+	 */
+	handlerEvent(event) {
+		const {subtype} = event;
+		const {BOT_ADD, APP_JOIN, CHANNEL_JOIN} = Env.chatServiceGOF("TYPE");
+		const {loginResource} = this.template;
+		switch (subtype) {
+			case BOT_ADD:
+				return handlerOptionLogin(event, loginResource);
+			case APP_JOIN:
+				return handlerOptionLogin(event, loginResource);
+			case CHANNEL_JOIN:
+			default:
+				return null;
+		}
 	}
 
-	resourceServerHandler(req, res, next){
+	async chatServiceHandler(req, res, next) {
+		let {challenge = null, event = null, payload = null, command = null} = req.body;
+		console.log(req.body);
+		try {
+			if (challenge) {
+				return res.status(200).send(challenge);
+			}
+			if (event) {
+				console.log('chatServiceHandler event: ', event);
+				const config = this.handlerEvent(event);
+				if(config) await Axios(config);
+			}
+			return res.status(200).send("OK");
+		} catch (e) {
+			console.log("chatServiceHandler error: ", e);
+			return res.status(400).send("ERROR");
+		}
+	}
+
+	resourceServerHandler(req, res, next) {
 		try {
 			proxy.web(req, res, {
 				target: 'http://localhost:5001'
@@ -30,16 +78,55 @@ class SlackWrapper extends BaseServer {
 			return res.status(204).send("ERROR")
 		}
 	}
+
+	loginWrapper(req, res, next) {
+		const {accessToken = null, redirect = null} = req.query;
+		try {
+			if (!accessToken || !redirect) return res.status(400).send("Bad request");
+			// res.cookie(`${redirect}_SLACK`, accessToken, {
+			// 	maxAge: expCookie,
+			// 	httpOnly: true,
+			// 	sameSite: "Strict",
+			// });
+			// switch (redirect) {
+			// 	case "GOOGLE":
+			// 		return res.status(307).redirect(configUrlAuthGoogle());
+			// 	case "MICROSOFT":
+			// 		return res.status(307).redirect(configUrlAuthMicrosoft());
+			// 	default:
+			// 		return res.status(400).send("Bad request");
+			// }
+			return res.status(200).send("OK");
+		} catch (e) {
+			return res.status(204).send("ERROR");
+		}
+	}
+
+	loginGoogle(req, res, next) {
+		proxy.web(req, res, {
+			target: `http:localhost:${Env.resourceServerGOF("GOOGLE_PORT")}`
+		})
+	}
+
+	loginMicrosoft(req, res, next) {
+		proxy.web(req, res, {
+			target: `http:localhost:${Env.resourceServerGOF("MICROSOFT_PORT")}`
+		})
+	}
 }
 
 module.exports = SlackWrapper;
 
-(async function() {
+(async function () {
 	const wrapper = new SlackWrapper(process.argv[2], {
 		config: {
 			path: process.argv[3],
 			appRoot: __dirname,
 		},
 	});
+	await Template().init();
 	await wrapper.init();
+	wrapper.app.get("/login-wrapper", wrapper.loginWrapper);
+	wrapper.app.get("/auth/google", wrapper.loginGoogle);
+	wrapper.app.get("/auth/microsoft", wrapper.loginMicrosoft);
 })();

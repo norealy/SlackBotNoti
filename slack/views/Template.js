6b751fef1f95cd9aa01cf2bea5ddf11da72dade1
SauSap <tuan.back@classfunc.com>
2021-03-07 22:04:27 +0700

    NEOS_VN_BNT-26 update

--- slack/views/Template.js
+++ slack/views/Template.js
@@ -3,35 +3,35 @@ const Fs = require('fs-extra');
 const Env = require('../../utils/Env');
 
 class TemplateSlack {
-  constructor() {
-    this.addEvent = null;
-    this.deleteEvent = null;
-    this.editEvent = null;
-    this.eventEndDate = null;
-    this.eventTimeStart = null;
-    this.eventTimeEnd = null;
-    this.homePage = null;
-    this.listCalendar = null;
-    this.listEvent = null;
-    this.loginResource = null;
-    this.showEvent = null;
-    this.systemSetting = null;
-    this.eventStartDate = null;
-  }
+	constructor() {
+		this.addEvent = null;
+		this.deleteEvent = null;
+		this.editEvent = null;
+		this.eventEndDate = null;
+		this.eventTimeStart = null;
+		this.eventTimeEnd = null;
+		this.homePage = null;
+		this.listCalendar = null;
+		this.listEvent = null;
+		this.loginResource = null;
+		this.showEvent = null;
+		this.systemSetting = null;
+		this.eventStartDate = null;
+	}
 
-  /**
-   * Read config file from config folder(either depending on the service you want to run)
-   * @param pathFile
-   * @returns {Promise<unknown>}
-   */
-  readFileTemplate(pathFile) {
-    return new Promise((resolve, reject) => {
-      Fs.readJson(Path.join(pathFile), (err, text) => {
-        if (err) reject(err);
-        resolve(text)
-      });
-    });
-  }
+	/**
+	 * Read config file from config folder(either depending on the service you want to run)
+	 * @param pathFile
+	 * @returns {Promise<unknown>}
+	 */
+	readFileTemplate(pathFile) {
+		return new Promise((resolve, reject) => {
+			Fs.readJson(Path.join(pathFile), (err, text) => {
+				if (err) reject(err);
+				resolve(text)
+			});
+		});
+	}
 
   /**
    * Add prefix for block_id and callback_id
@@ -80,9 +80,9 @@ class TemplateSlack {
    * @param {string} id
    * @return {string}
    */
-  getBlockId(id) {
-    return `${this._prefix}_${id}`
-  }
+	getBlockId(id) {
+		return `${this._prefix}_${id}`
+	}
 
   /**
    * Create time picker
@@ -132,44 +132,42 @@ class TemplateSlack {
    * @param {string} prefix
    * @return {TemplateSlack}
    */
-  async init(prefix) {
-    try {
-      let pathFile = Env.appRoot + '/slack/views';
-      this.addEvent = await this.readFileTemplate(`${pathFile}/AddEvent.json`);
-      let timeStart = [...this._customDatetime()];
+	async init(prefix) {
+		try {
+			let pathFile = Env.appRoot + '/slack/views';
+			this.addEvent = await this.readFileTemplate(`${pathFile}/AddEvent.json`);
+			let timeStart = [...this._customDatetime()];
       const timeEnd = [...timeStart];
       timeStart.pop();
       timeEnd.shift();
-      this.addEvent.blocks[6].accessory.options = timeStart;
-      this.addEvent.blocks[7].accessory.options = timeEnd;
-      this.deleteEvent = await this.readFileTemplate(`${pathFile}/DeleteEvent.json`);
-      this.editEvent = await this.readFileTemplate(`${pathFile}/EditEvent.json`);
-      this.editEvent.blocks[6].accessory.options = timeStart;
-      this.editEvent.blocks[7].accessory.options = timeEnd;
-      this.eventEndDate = await this.readFileTemplate(`${pathFile}/EventEndDate.json`);
-      this.eventTimeStart = await this.readFileTemplate(`${pathFile}/EventTimeStart.json`);
-      this.eventTimeEnd = await this.readFileTemplate(`${pathFile}/EventTimeEnd.json`);
-      this.homePage = await this.readFileTemplate(`${pathFile}/HomePage.json`);
-      this.listCalendar = await this.readFileTemplate(`${pathFile}/ListCalendar.json`);
-      this.listEvent = await this.readFileTemplate(`${pathFile}/ListEvent.json`);
-      this.loginResource = await this.readFileTemplate(`${pathFile}/LoginResource.json`);
-      this.showEvent = await this.readFileTemplate(`${pathFile}/ShowEvent.json`);
-      this.systemSetting = await this.readFileTemplate(`${pathFile}/SystemSetting.json`);
-      this.eventStartDate = await this.readFileTemplate(`${pathFile}/EventStartDate.json`);
-      this._renameBlockAndCallbackID(prefix);
-      this._prefix = prefix;
-      return this;
-    } catch (err) {
-      throw err
-    }
-  }
+			this.addEvent.blocks[6].accessory.options = timeStart;
+			this.addEvent.blocks[7].accessory.options = timeEnd;
+			this.deleteEvent = await this.readFileTemplate(`${pathFile}/DeleteEvent.json`);
+			this.editEvent = await this.readFileTemplate(`${pathFile}/EditEvent.json`);
+			this.eventEndDate = await this.readFileTemplate(`${pathFile}/EventEndDate.json`);
+			this.eventTimeStart = await this.readFileTemplate(`${pathFile}/EventTimeStart.json`);
+			this.eventTimeEnd = await this.readFileTemplate(`${pathFile}/EventTimeEnd.json`);
+			this.homePage = await this.readFileTemplate(`${pathFile}/HomePage.json`);
+			this.listCalendar = await this.readFileTemplate(`${pathFile}/ListCalendar.json`);
+			this.listEvent = await this.readFileTemplate(`${pathFile}/ListEvent.json`);
+			this.loginResource = await this.readFileTemplate(`${pathFile}/LoginResource.json`);
+			this.showEvent = await this.readFileTemplate(`${pathFile}/ShowEvent.json`);
+			this.systemSetting = await this.readFileTemplate(`${pathFile}/SystemSetting.json`);
+			this.eventStartDate = await this.readFileTemplate(`${pathFile}/EventStartDate.json`);
+			this._renameBlockAndCallbackID(prefix);
+			this._prefix = prefix;
+			return this;
+		} catch (err) {
+			throw err
+		}
+	}
 }
 
 let template = "";
 module.exports =  function () {
-  if(!template) {
-    template = new TemplateSlack();
-    return template
-  }
-  return template
+	if(!template) {
+		template = new TemplateSlack();
+		return template
+	}
+	return template
 };

e41471bdecc4dd049be9d8cc300dace1ea1732bf
Tuấn Nguyễn <tuannv@neoscorp.vn>
2021-03-08 09:28:37 +0700

    NEOS_VN_BNT-38 format code folder database/migrations, models/mongo, utils

--- database/migrations/20210129100550_GoogleAccount.js
+++ database/migrations/20210129100550_GoogleAccount.js
@@ -1,14 +1,14 @@
-exports.up = function(knex) {
-    return knex.schema.createTable('google_account', function(table) {
-        table.string('id').primary().notNull();
-        table.string('name').notNull();
-        table.text('refresh_token').notNull();
-        table.string('timezone').notNull();
-        table.timestamp('created_at').defaultTo(knex.fn.now());
-        table.timestamp('updated_at').defaultTo(knex.fn.now());
-    });
+exports.up = function (knex) {
+  return knex.schema.createTable('google_account', function (table) {
+    table.string('id').primary().notNull();
+    table.string('name').notNull();
+    table.text('refresh_token').notNull();
+    table.string('timezone').notNull();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
 };
 
-exports.down = function(knex) {
-    return knex.schema.dropTable('google_account');
+exports.down = function (knex) {
+  return knex.schema.dropTable('google_account');
 };
--- database/migrations/20210129100629_MicrosoftAccountCalendar.js
+++ database/migrations/20210129100629_MicrosoftAccountCalendar.js
@@ -1,13 +1,12 @@
-
-exports.up = function(knex) {
-    return knex.schema.createTable('microsoft_account_calendar', function(table) {
-        table.string('id_calendar').references('id').inTable('microsoft_calendar').notNull();
-        table.string('id_account').references('id').inTable('microsoft_account').notNull();
-        table.primary(['id_calendar', 'id_account']);
-        table.timestamp('created_at').defaultTo(knex.fn.now());
-    });
+exports.up = function (knex) {
+  return knex.schema.createTable('microsoft_account_calendar', function (table) {
+    table.string('id_calendar').references('id').inTable('microsoft_calendar').notNull();
+    table.string('id_account').references('id').inTable('microsoft_account').notNull();
+    table.primary(['id_calendar', 'id_account']);
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+  });
 };
 
-exports.down = function(knex) {
-    return knex.schema.dropTable('microsoft_account_calendar');
+exports.down = function (knex) {
+  return knex.schema.dropTable('microsoft_account_calendar');
 };
